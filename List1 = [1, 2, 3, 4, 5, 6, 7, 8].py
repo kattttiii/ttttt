e25a2b77c8afc22c8de155f578223e55a362f226
kattttiii <144277414+kattttiii@users.noreply.github.com>
2023-09-28 15:08:45 +0300

    Add files via upload

---
+++ List1 = [1, 2, 3, 4, 5, 6, 7, 8].py
@@ -0,0 +1,23 @@
+List1 = [1, 2, 3, 4, 5, 6, 7, 8]
+print ("Сумма чисел:", sum(List1))
+
+List2 = [4, 7, 25, 66, 99, -34, 5, -23]
+print ("Наибольший элемент:", max(List2))
+
+List3 = [5, 6, 9, 15, 5, 7, 44, 15, 22]
+print ("Уникальные элементы:", set(List3))
+
+List4 = [1, 5, 3, 12]
+List5 = [2, 4, 6]
+dvaspiska = List4 + List5 
+print ("Объединенный список:", dvaspiska)
+
+food = ("cucumber", "bananas", "tomatoes", "apple")
+index = food.index("apple")
+print("Индекс:", index)
+
+List6 = [1, 2, 3, 4]
+List7 = [5, 6, 7, 8]
+dvakortega = List6 + List7 
+print("Два кортежа:", str("dvakortega")) 
+
